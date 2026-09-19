026dbd9b8cc885523dfab62dc5ed8fcf32f05e90
octavia <kiranabjb09@gmail.com>
2026-09-19 15:48:01 +0800

    Coding Biodata

---
+++ Biodata.java
@@ -0,0 +1,15 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String Nama = "Octavia Putri Kirana";
+        String Fakultas = "Fakultas Matematika dan Ilmu Pengetahuan Alam";
+        String Jurusan = "Ilmu Komputer";
+        String Angkatan = "2026";
+
+        System.out.println("=== BIODATA ===");
+        System.out.println("Nama    : " + Nama);
+        System.out.println("Fakultas: " + Fakultas);
+        System.out.println("Jurusan : " + Jurusan);
+        System.out.println("Angkatan: " + Angkatan);
+    }
+}
+
